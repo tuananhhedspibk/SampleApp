bc8bf06453b1a2e9d7025e2fb3fc1314f80ee50a
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-09-18 22:14:12 +0700

    Fix route file error with contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	root 'static_pages#home'
-	get 'contact', to: 'static_pages#contact'
+	get '/contact', to: 'static_pages#contact'
 	get '/about', to: 'static_pages#about'
 	get '/help', to: 'static_pages#help'
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
