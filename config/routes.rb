5cfa64b8c40fc1d1405962fb8f22bfde56154453
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-11-08 21:39:33 +0700

    Fix route error

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 	root 'static_pages#home'
 	get '/contact', to: 'static_pages#contact'
 	get '/about', to: 'static_pages#about'
-	get '/help', to: 'static_pags#help'
+	get '/help', to: 'static_pages#help'
 	get '/signup', to: 'users#new'
 	post '/signup',  to: 'users#create'
 	get    '/login',   to: 'sessions#new'
